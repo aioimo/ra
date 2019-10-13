4c4432e420b1758096bff6feb219734ffac474fe
Anthony Ioimo <anthonylioimo@gmail.com>
2019-10-13 15:02:31 +0200

    basic App structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+
+import * as pages from './pages';
+
+import { auth } from './lib/auth';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <nav className='Nav'>
+        {auth.isLoggedIn() ? (
+          <>Logged in as {auth.getEmail()}</>
+        ) : (
+          <>
+            <Link to='/signup'>Signup</Link>
+            <Link to='/login'>Login</Link>
+          </>
+        )}
+      </nav>
+      {auth.isLoggedIn() ? (
+        <Switch>
+          <Route exact path='/login' render={() => <Redirect to='/' />} />
+          <Route
+            path='/'
+            render={props => <pages.Home {...props} auth={auth} />}
+          />
+        </Switch>
+      ) : (
+        <Switch>
+          <Route
+            exact
+            path='/signup'
+            render={props => <pages.Signup {...props} auth={auth} />}
+          />
+          <Route
+            exact
+            path='/login'
+            render={props => <pages.Login {...props} auth={auth} />}
+          />
+        </Switch>
+      )}
     </div>
   );
 }
