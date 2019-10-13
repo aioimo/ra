79d5c0e3d48da61ef3741cd6333050b62238e394
Anthony Ioimo <anthonylioimo@gmail.com>
2019-10-13 15:00:39 +0200

    Login page

---
+++ src/pages/Login/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+export const Login = ({ auth }) => {
+  const [password, setPassword] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const login = e => {
+    e.preventDefault();
+    console.log('login callled...', { password, email });
+    auth
+      .login({ password, email })
+      .then(res => {
+        window.location.reload();
+      })
+      .catch(err => {
+        console.log('err....', err);
+      });
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <form onSubmit={login}>
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          type='text'
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+        <label htmlFor='password'>Password</label>
+        <input
+          id='password'
+          type='password'
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+        />
+        <button type='submit'>Submit</button>
+      </form>
+    </div>
+  );
+};
